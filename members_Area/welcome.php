30bb4b1a5c75eabd577831af85ed9a2189f2fdc3
Cubic-Mc <param4mc@gmail.com>
2024-03-19 00:26:33 +0530

    chatbot integration

--- members_Area/welcome.php
+++ members_Area/welcome.php
@@ -7,6 +7,12 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Doubt forum </title>
+    <link rel="stylesheet" href="/PICT_doubt/chat bot API/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google Fonts Link For Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0" />
+    <script src="/PICT_doubt/chat bot API/script.js" defer></script>
    <link rel="stylesheet" href="members.css">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
@@ -37,10 +43,25 @@ session_start();
         <img src="/PICT_doubt/Pict gate 3.jpg" alt="Centred Image">
         <!-- <figcaption>This is a caption for the image.</figcaption> -->
     </figure>
-    <div class="chat-box">
-        <div class="chat-icon">
-            <a href="/PICT_doubt/chatbot/chatbot.html"><img src="/PICT_doubt/chat-icon.jpeg" alt="Chat Icon"></a>
+    <button class="chatbot-toggler">
+        <span class="material-symbols-rounded">mode_comment</span>
+        <span class="material-symbols-outlined">close</span>
+      </button>
+      <div class="chatbot">
+        <header>
+          <h2>Chatbot</h2>
+          <span class="close-btn material-symbols-outlined">close</span>
+        </header>
+        <ul class="chatbox">
+          <li class="chat incoming">
+            <span class="material-symbols-outlined">smart_toy</span>
+            <p>Hi there 👋<br>How can I help you today?</p>
+          </li>
+        </ul>
+        <div class="chat-input">
+          <textarea placeholder="Enter a message..." spellcheck="false" required></textarea>
+          <span id="send-btn" class="material-symbols-rounded">send</span>
         </div>
-    </div>
+      </div>
 </body>
 </html>
