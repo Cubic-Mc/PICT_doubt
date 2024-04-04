86afa79c2ea9093ca03acfbdeee98187267b14c4
Cubic-Mc <param4mc@gmail.com>
2024-04-04 21:53:29 +0530

    test

---
+++ test_code/script.js
@@ -0,0 +1,10 @@
+// This is an example of a JavaScript function that gets called when the button is clicked
+// You can replace this with your own functionality
+function askNow() {
+    alert("You clicked the Ask Now button!");
+  }
+  
+  // Add an event listener to the button to call the askNow function when it's clicked
+  const button = document.querySelector('button');
+  button.addEventListener('click', askNow);
+  
